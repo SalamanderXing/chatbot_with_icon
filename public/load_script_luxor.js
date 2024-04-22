77f9b33973c0645a372026a73c5d3cfcd84ed7e5
salamanderxing <giulio.zani@gmail.com>
2024-04-22 22:13:30 +0200

    improved language support

--- public/load_script_luxor.js
+++ public/load_script_luxor.js
@@ -1,22 +1,19 @@
 (async () => {
-    alert('hey')
     const response = await fetch("https://raw.githubusercontent.com/SalamanderXing/chatbot_with_icon/main/public/luxor.html");
     const htmlContent = await response.text();
-    const scriptTag = document.querySelector("#chatbot_script");
-    setTimeout(() => {
-        const body = document.body
-        body.insertAdjacentHTML('beforeend', htmlContent);
-        const chatbotContainer = document.querySelector("#chatbot_container");
-        Array.from(
-            chatbotContainer.querySelectorAll("script"),
-        ).forEach((oldScript) => {
-            const newScript = document.createElement("script");
-            if (oldScript.src) {
-                newScript.src = oldScript.src;
-            } else {
-                newScript.textContent = oldScript.textContent;
-            }
-            oldScript.parentNode.replaceChild(newScript, oldScript);
-        });
-    }, 1000)
+    //const scriptTag = document.querySelector("#chatbot_script");
+    const body = document.body
+    body.insertAdjacentHTML('beforeend', htmlContent);
+    const chatbotContainer = document.querySelector("#chatbot_container");
+    Array.from(
+        chatbotContainer.querySelectorAll("script"),
+    ).forEach((oldScript) => {
+        const newScript = document.createElement("script");
+        if (oldScript.src) {
+            newScript.src = oldScript.src;
+        } else {
+            newScript.textContent = oldScript.textContent;
+        }
+        oldScript.parentNode.replaceChild(newScript, oldScript);
+    });
 })();
